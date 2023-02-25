ba8f13ad27bc8be81dfe929a73450f250c068e6b
Suluoya <1931960436@qq.com>
2023-02-26 01:03:12 +0800

    test

--- test.py
+++ test.py
@@ -2,15 +2,16 @@
 # @Author: 昵称有六个字
 # @Date:   2023-02-22 23:37:51
 # @Last Modified by:   昵称有六个字
-# @Last Modified time: 2023-02-25 22:43:07
+# @Last Modified time: 2023-02-26 00:56:57
 import pandas as pd
 from icecream import ic
 
 ic.configureOutput("")
 
 
-df_class = pd.read_excel("origin/class.xlsx")
-df_internet = pd.read_csv("controls/ISI.csv")
-df_class = pd.merge(df_class, df_internet, on="province", how="left")
+# df_class = pd.read_excel("origin/class.xlsx")
+# df_internet = pd.read_csv("controls/ISI.csv")
+# df_class = pd.merge(df_class, df_internet, on="province", how="left")
 
-df_class.to_excel("origin/class.xlsx", index=False)
+# df_class.to_excel("origin/class.xlsx", index=False)
+ic(1)
